ae155146d2298c69a39124e71c5435e327e49b14
Dialu <1165011707@qq.com>
2024-11-12 12:59:53 +0800

    添加 更新路线图和版本号

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 // appVersion 软件版本号
-const AppVersion = "v1.0.0"
+const AppVersion = "v1.1.0"
 
 // githubURL 项目github仓库链接
 const GithubURL = "https://github.com/Dialu41/MapPhotoMD"
